d39b19c93896839dfa8f0d1a353fbd818c16d8a1
Austin Lasseter <austinlasseter@gmail.com>
2018-05-06 21:22:34 -0400

    remove debug

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ app.layout = html.Div(children=[
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
